f682b6348f0481d6a18b7890c7f0b06628c2c8a4
DreadfulCrazy <lucasospina97@gmail.com>
2022-05-26 00:57:56 -0500

    Observer y suscribe

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
-import { Observable } from 'rxjs'
+import { Observable, Observer } from 'rxjs'
 
 // const obs$ = Observable.create()
+
+const observer: Observer<string>  = {
+    next: value => console.log("Next: ", value),
+    error: error => console.warn("Error: ", error),
+    complete: () => console.info("completado") 
+}
 
 const obs$ = new Observable<string>( subs => {
     subs.next('Hola')
@@ -9,10 +15,23 @@ const obs$ = new Observable<string>( subs => {
     subs.next('Hola')
     subs.next('Mundo')
 
+    // Forzar error
+    // const a = undefined
+    // a.nombre = "lucas"
+
     subs.complete()
-    
+
 });
 
+obs$.subscribe(observer)
 
-obs$.subscribe(console.log)
+/**
+ * Hay una tercera forma de hacer esto mismo y esa funcionalidad
+ * llamamos observer --> linea 5
+ */
+// obs$.subscribe(
+//     valor => console.log('next', valor),
+//     error => console.warn('error', error),
+//     () => console.info('complete')
+// )
 
